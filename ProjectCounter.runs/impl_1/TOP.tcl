2093208bab9fff8989fbdd14ea2c3dc19ce64e4d
andreimocian <andrei_mocian@yahoo.com>
2024-06-27 21:12:16 +0300

    Added documentation

--- ProjectCounter.runs/impl_1/TOP.tcl
+++ ProjectCounter.runs/impl_1/TOP.tcl
@@ -44,16 +44,95 @@ proc step_failed { step } {
 
 set_msg_config -id {HDL 9-1061} -limit 100000
 set_msg_config -id {HDL 9-1654} -limit 100000
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
+
+start_step init_design
+set ACTIVE_STEP init_design
+set rc [catch {
+  create_msg_db init_design.pb
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
+  set_property webtalk.parent_dir D:/UTCN/Sem2/DSD/Labs/Digital_Timer_Mocian_Andrei/Digital_Timer/ProjectCounter.cache/wt [current_project]
+  set_property parent.project_path D:/UTCN/Sem2/DSD/Labs/Digital_Timer_Mocian_Andrei/Digital_Timer/ProjectCounter.xpr [current_project]
+  set_property ip_output_repo D:/UTCN/Sem2/DSD/Labs/Digital_Timer_Mocian_Andrei/Digital_Timer/ProjectCounter.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  add_files -quiet D:/UTCN/Sem2/DSD/Labs/Digital_Timer_Mocian_Andrei/Digital_Timer/ProjectCounter.runs/synth_1/TOP.dcp
+  read_xdc {{D:/UTCN/Sem2/DSD/Labs/Digital_Timer_Mocian_Andrei/Digital_Timer/ProjectCounter.srcs/constrs_1/imports/Constraint files/Basys3_Master.xdc}}
+  link_design -top TOP -part xc7a35tcpg236-1
+  write_hwdef -file TOP.hwdef
+  close_msg_db -file init_design.pb
+} RESULT]
+if {$rc} {
+  step_failed init_design
+  return -code error $RESULT
+} else {
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force TOP_opt.dcp
+  catch { report_drc -file TOP_drc_opted.rpt }
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  implement_debug_core 
+  place_design 
+  write_checkpoint -force TOP_placed.dcp
+  catch { report_io -file TOP_io_placed.rpt }
+  catch { report_utilization -file TOP_utilization_placed.rpt -pb TOP_utilization_placed.pb }
+  catch { report_control_sets -verbose -file TOP_control_sets_placed.rpt }
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force TOP_routed.dcp
+  catch { report_drc -file TOP_drc_routed.rpt -pb TOP_drc_routed.pb -rpx TOP_drc_routed.rpx }
+  catch { report_methodology -file TOP_methodology_drc_routed.rpt -rpx TOP_methodology_drc_routed.rpx }
+  catch { report_timing_summary -warn_on_violation -max_paths 10 -file TOP_timing_summary_routed.rpt -rpx TOP_timing_summary_routed.rpx }
+  catch { report_power -file TOP_power_routed.rpt -pb TOP_power_summary_routed.pb -rpx TOP_power_routed.rpx }
+  catch { report_route_status -file TOP_route_status.rpt -pb TOP_route_status.pb }
+  catch { report_clock_utilization -file TOP_clock_utilization_routed.rpt }
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force TOP_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
+  unset ACTIVE_STEP 
+}
 
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param xicom.use_bs_reader 1
-  open_checkpoint TOP_routed.dcp
-  set_property webtalk.parent_dir D:/UTCN/Sem2/DSD/Labs/Digital_Timer/ProjectCounter.cache/wt [current_project]
   catch { write_mem_info -force TOP.mmi }
   write_bitstream -force -no_partial_bitfile TOP.bit 
   catch { write_sysdef -hwdef TOP.hwdef -bitfile TOP.bit -meminfo TOP.mmi -file TOP.sysdef }
--- ProjectCounter.runs/synth_1/.Xil/TOP_propImpl.xdc
+++ ProjectCounter.runs/synth_1/.Xil/TOP_propImpl.xdc
@@ -1,4 +1,4 @@
-set_property SRC_FILE_INFO {cfile:{D:/UTCN/Sem2/DSD/Labs/Digital_Timer/ProjectCounter.srcs/constrs_1/imports/Constraint files/Basys3_Master.xdc} rfile:{../../../ProjectCounter.srcs/constrs_1/imports/Constraint files/Basys3_Master.xdc} id:1} [current_design]
+set_property SRC_FILE_INFO {cfile:{D:/UTCN/Sem2/DSD/Labs/Digital_Timer_Mocian_Andrei/Digital_Timer/ProjectCounter.srcs/constrs_1/imports/Constraint files/Basys3_Master.xdc} rfile:{../../../ProjectCounter.srcs/constrs_1/imports/Constraint files/Basys3_Master.xdc} id:1} [current_design]
 set_property src_info {type:XDC file:1 line:7 export:INPUT save:INPUT read:READ} [current_design]
 set_property -dict { PACKAGE_PIN W5   IOSTANDARD LVCMOS33 } [get_ports CLK]
 set_property src_info {type:XDC file:1 line:12 export:INPUT save:INPUT read:READ} [current_design]
